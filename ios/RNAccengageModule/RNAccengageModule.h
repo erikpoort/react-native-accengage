6e65c48a17294503c0518a6aa71233967aaad42c
Erik Poort <erik@mediamonks.com>
2017-07-28 14:53:26 -0300

    Add tracking methods iOS

--- ios/RNAccengageModule/RNAccengageModule.h
+++ ios/RNAccengageModule/RNAccengageModule.h
@@ -7,7 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
 
-@interface RNAccengageModule : NSObject
+@interface RNAccengageModule : NSObject <RCTBridgeModule>
 
 @end
--- ios/RNAccengageModule/RNAccengageModule.m
+++ ios/RNAccengageModule/RNAccengageModule.m
@@ -7,7 +7,41 @@
 //
 
 #import "RNAccengageModule.h"
+#import <Accengage/Accengage.h>
+
+static NSString *const kRejectCode = @"RNAccengageModule.h";
 
 @implementation RNAccengageModule
+RCT_EXPORT_MODULE();
+
+#pragma mark - Tracking
+
+RCT_EXPORT_METHOD(
+    trackEvent:(NSInteger)key
+) {
+    [Accengage trackEvent:key];
+}
+
+RCT_EXPORT_METHOD(
+    trackEventWithCustomData:(NSInteger)key
+    customData:(NSDictionary *)customData
+) {
+    if (!customData) {
+        [Accengage trackEvent:key];
+        return;
+    }
+    
+    NSError *error = nil;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:customData options:0 error:&error];
+    
+    if (error) {
+        NSLog(@"Custom data is sent in unsuported type and ignored");
+        [Accengage trackEvent:key];
+        return;
+    }
+    
+    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    [Accengage trackEvent:key withParameters:@[jsonString]];
+}
 
 @end
