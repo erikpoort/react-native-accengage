7306004534670761f8df6e944b9a9448ce09edbb
Erik Poort <erik@mediamonks.com>
2017-07-28 13:06:06 -0300

    Add initial Android classes

---
+++ android/src/main/java/com/mediamonks/rnaccengage/RNApptentivePackage.java
@@ -0,0 +1,40 @@
+package com.mediamonks.rnaccengage;
+
+import android.app.Application;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by erik on 28/07/2017.
+ * MediaMonks 2017
+ */
+
+public class RNAccengagePackage implements ReactPackage
+{
+	@Override
+	public List<NativeModule> createNativeModules(ReactApplicationContext reactContext)
+	{
+		List<NativeModule> modules = new ArrayList<>();
+		return modules;
+	}
+
+	@Override
+	public List<Class<? extends JavaScriptModule>> createJSModules()
+	{
+		return Collections.emptyList();
+	}
+
+	@Override
+	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext)
+	{
+		return Collections.emptyList();
+	}
+}
