e111ac2b23475f01572e40ff76882cf93e2abb7f
Erik Poort <erik@mediamonks.com>
2017-07-28 13:00:07 -0300

    Add initial iOS classes

---
+++ ios/RNAccengageModule/RNAccengageModule.h
@@ -0,0 +1,13 @@
+//
+//  RNAccengageModule.h
+//  RNAccengageModule
+//
+//  Created by Erik Poort on 28/07/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNAccengageModule : NSObject
+
+@end
---
+++ ios/RNAccengageModule/RNAccengageModule.m
@@ -0,0 +1,13 @@
+//
+//  RNAccengageModule.m
+//  RNAccengageModule
+//
+//  Created by Erik Poort on 28/07/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import "RNAccengageModule.h"
+
+@implementation RNAccengageModule
+
+@end
